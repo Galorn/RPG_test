5e2150766440f84cf875fc67c2a9eca1d4cedd30
Galorn <alexis.teston@ecam-strasbourg.eu>
2016-03-05 17:15:27 +0100

    Intéraction utilisateur
    Intéraction concernant le personnage pour le nom, l'arme et les dégats
    de celle-ci.
    Future implémentation : Liste arme possible + dégats, choix de
    l'utilisateur, choix des actions possibles par celui-ci.

--- main.cpp
+++ main.cpp
@@ -6,9 +6,24 @@ using namespace std;
 
 int main()
 {
-
+    string nomUn, nomDeux, armeUn, armeDeux;
+    int degatArmeUn, degatArmeDeux;
+    //PREMIER COMBATTANT
+    cout << "Nommez votre premier combattant" << endl;
+    cin >> nomUn ;
+    cout << "Choisissez votre arme" << endl;
+    cin >> armeUn;
+    cout << "Combien de degats fait-elle ?" << endl;
+    cin >> degatArmeUn;
+    //DEUXIEME COMBATTANT
+    cout << "Nommez votre deuxième combattant" << endl;
+    cin >> nomDeux ;
+    cout << "Choisissez votre arme" << endl;
+    cin >> armeDeux;
+    cout << "Combien de degats fait-elle ?" << endl;
+    cin >> degatArmeDeux;
     // Création des personnages
-    Personnage deuxiemeperso ("Jacques", "son poing", 10),premierperso("Paul", "Epée aiguisée", 20);
+    Personnage deuxiemeperso (nomUn, armeUn, degatArmeUn),premierperso(nomDeux, armeDeux, degatArmeDeux);
 
     // Au combat !
     premierperso.attaquer(deuxiemeperso);
